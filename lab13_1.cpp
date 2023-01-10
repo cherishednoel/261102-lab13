87ab0fa478693ae282ba3c8c463bfe408ccf59b9
cherishednoel <panida.stp@icloud.com>
2023-01-10 15:23:06 +0700

    lab13_1.cpp completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
+long long int fibonacci(int);
+
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
+}
+
+long long int fibonacci(int n) {
+    if(n == 0 || n == 1) {
+        return n;
+    } else {
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
 }
