8488dd256070000c3a3ddd43f77c45f9698e2051
Panida Noel Suthapakti <110964402+cherishednoel@users.noreply.github.com>
2023-01-13 08:16:55 +0700

    ***don't test

---
+++ exercise11_3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+long long int sequence1(int);
+double sequence2(int);
+double sequence3(int);
+
+int main() {
+    cout << sequence1(4) << endl;
+    cout << sequence2(4) << endl;
+    cout << sequence3(4) << endl;
+    return 0;
+}
+
+long long int sequence1(int n) {
+    if (n == 1)
+        return 2;
+    else
+        return pow(2,n) + sequence1(n - 1);
+}
+
+double sequence2(int n) {
+    if (n == 1)
+        return 1;
+    else
+        return sqrt(n) * sequence2(n - 1);
+}
+
+double sequence3(int n) {
+    if (n == 1)
+        return 2;
+    else
+        return 1 + (n / sequence3(n-1));
+}
